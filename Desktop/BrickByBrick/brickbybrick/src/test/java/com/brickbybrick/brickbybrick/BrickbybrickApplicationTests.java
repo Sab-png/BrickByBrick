64536135cb792149a2e65c4f47f0d5145b815561
Sab-png <andrea.sabini@edu-its.it>
2025-10-28 16:31:57 +0100

    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ Desktop/BrickByBrick/brickbybrick/src/test/java/com/brickbybrick/brickbybrick/BrickbybrickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brickbybrick.brickbybrick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickbybrickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
