0edd992a84f9353979fe8624f6b0874e4cd1ecc5
daniela <daniela.punzi@edu-its.it>
2025-10-29 11:33:43 +0100

    creazione di Utente.java all'interno della cartela model + inserimento degli attributi + inserimento dei get e set

---
+++ Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/model/Utente.java
@@ -0,0 +1,106 @@
+package com.brickbybrick.brickbybrick.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "utente")
+public class Utente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id_utente")
+    private Integer Id_utente;
+
+    @Column(name = "Id_ruolo", nullable = false)
+    private Integer Id_ruolo;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "cognome", nullable = false)
+    private String cognome;
+
+    @Column(name = "telefono", length = 15)
+    private String telefono;
+
+    @Column(name = "codice_fiscale", length = 16)
+    private String codice_fiscale;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+
+    public Integer getId_utente() {
+        return Id_utente;
+    }
+
+    public void setId_utente(Integer id_utente) {
+        Id_utente = id_utente;
+    }
+
+    public Integer getId_ruolo() {
+        return Id_ruolo;
+    }
+
+    public void setId_ruolo(Integer id_ruolo) {
+        Id_ruolo = id_ruolo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCodice_fiscale() {
+        return codice_fiscale;
+    }
+
+    public void setCodice_fiscale(String codice_fiscale) {
+        this.codice_fiscale = codice_fiscale;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
