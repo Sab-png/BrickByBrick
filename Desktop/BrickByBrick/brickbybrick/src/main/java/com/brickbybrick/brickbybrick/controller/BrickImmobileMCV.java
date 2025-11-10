0be4a974c1cfd444f3a3c947c7ec7e484d523970
daniela <daniela.punzi@edu-its.it>
2025-11-10 16:14:45 +0100

    implementazione del metodo getImmobili in BrickServiceImmmobile, BrickServiceImmobileImpl e BrickImmobileMVC

--- Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/controller/BrickImmobileMCV.java
+++ Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/controller/BrickImmobileMCV.java
@@ -2,15 +2,32 @@ package com.brickbybrick.brickbybrick.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 import com.brickbybrick.brickbybrick.repos.BrickRepoImmobile;
+import com.brickbybrick.brickbybrick.services.BrickServiceImmobile;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @Controller
 public class BrickImmobileMCV {
-    private final BrickRepoImmobile repoImmobile;
     @Autowired
-    private BrickRepoImmobile serviceImmobile;
+    private final BrickRepoImmobile repoImmobile;
+
+    @Autowired
+    private BrickServiceImmobile serviceImmobile;
+
     public BrickImmobileMCV(BrickRepoImmobile repoImmobile) {
         this.repoImmobile = repoImmobile;
     }
+
+    @GetMapping("/immobili")
+    public String getImmobili(Model m) {
+        m.addAttribute("immobili", serviceImmobile.getImmobili());
+        return "Immobili";
+    }
+    
 
 }
--- Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/services/BrickServiceImmobile.java
+++ Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/services/BrickServiceImmobile.java
@@ -1,5 +1,11 @@
 package com.brickbybrick.brickbybrick.services;
 
+import java.util.List;
+
+import com.brickbybrick.brickbybrick.model.Immobile;
+
 public interface BrickServiceImmobile {
+
+    List<Immobile> getImmobili();
 
 }
