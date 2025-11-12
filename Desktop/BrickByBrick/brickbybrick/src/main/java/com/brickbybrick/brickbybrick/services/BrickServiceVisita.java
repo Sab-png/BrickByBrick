1f336e938bb4c757cae9260e56fbb2a1bc0efa1e
daniela <daniela.punzi@edu-its.it>
2025-11-12 12:06:27 +0100

    implementazione del metodo edit per l'agente + creazione del file AgenteEdit per testare

--- Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/services/BrickServiceVisita.java
+++ Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/services/BrickServiceVisita.java
@@ -1,24 +1,23 @@
-package com.brickbybrick.brickbybrick.services;
+// package com.brickbybrick.brickbybrick.services;
 
-import java.util.List;
+// import java.util.List;
+// import com.brickbybrick.brickbybrick.model.Visita;
 
-import com.brickbybrick.brickbybrick.model.Visita;
+// public interface BrickServiceVisita {
 
-public interface BrickServiceVisita {
+//     List<Visita> getVisite();
 
-    List<Visita> getVisite();
+//     Visita addVisita(Visita v);
 
-    Visita addVisita(Visita v);
+//     Visita getVisitaById(Integer id);
 
-    Visita getVisitaById(Integer id);
+//     List<Visita> getVisiteByImmobile(Integer idImmobile);
 
-    List<Visita> getVisiteByImmobile(Integer idImmobile);
+//     List<Visita> getVisiteByAgente(Integer idAgente);
 
-    List<Visita> getVisiteByAgente(Integer idAgente);
+//     Visita updateVisita(Integer id, Visita v);
 
-    Visita updateVisita(Integer id, Visita v);
+//     void deleteVisita(Integer id);
 
-    void deleteVisita(Integer id);
-
-    void deleteAllVisite();
-}
+//     void deleteAllVisite();
+// }
