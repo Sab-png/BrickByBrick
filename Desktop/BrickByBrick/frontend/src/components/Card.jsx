c7027a0d39139942684512e62b5f5929b5764356
Jacopo Dell'Oste <jacodell25@gmail.com>
2025-11-13 22:05:39 +0100

    feat: Card component's completed

--- Desktop/BrickByBrick/frontend/src/components/Card.jsx
+++ Desktop/BrickByBrick/frontend/src/components/Card.jsx
@@ -1,15 +1,12 @@
-import '../styles/components/_card.scss';
-export default function Card({image, subtitle,text}) {
-
-    return (
-        <div className="card-container">
-            <div className="card-image-wrapper">
-                <img src={image} alt={subtitle} className="card-image" />
-            </div>
-            <h3 className="card-subtitle">{subtitle}</h3>
-            <p className="card-text">{text}</p>
-        </div>
-        
-    )
-
+export default function Card({ image, subtitle, text }) {
+  return (
+    <div className="card">
+      <div className="card__image-wrapper">
+        <img src={image} alt={subtitle} className="card__image" />
+      </div>
+      <h3 className="card__subtitle">{subtitle}</h3>
+      <p className="card__text">{text}</p>
+    </div>
+  );
 }
+
