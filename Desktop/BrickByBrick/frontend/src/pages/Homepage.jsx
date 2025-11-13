cbe66ed9ed47b4e011beaecb00b6d400c626bbab
Jacopo Dell'Oste <jacodell25@gmail.com>
2025-11-13 22:27:17 +0100

    feat: homepage completed

--- Desktop/BrickByBrick/frontend/src/pages/Homepage.jsx
+++ Desktop/BrickByBrick/frontend/src/pages/Homepage.jsx
@@ -1,17 +1,20 @@
 import HeroSection from "../components/HeroSection"
 import FeaturesSection from "../components/FeatureSection"
-import Card from "../components/Card"
 import Newsletter from "../components/NewsLetter"
 import CardSection from "../components/CardSection"
+
 export default function Homepage() {
     return (
-        <>
-      <HeroSection />
-      <FeaturesSection/>
-      <CardSection />
-      <Newsletter />
+      <>
+      <HeroSection title={"Vendi la tua casa in Piemonte con un valore garantito e senza stress."}
+      bgImage="/src/assets/images/hero-homepage.jpg"/>
 
-    </>
+      <FeaturesSection/>
+
+      <CardSection />
+
+      <Newsletter />
+      </>
 
     )
 }
