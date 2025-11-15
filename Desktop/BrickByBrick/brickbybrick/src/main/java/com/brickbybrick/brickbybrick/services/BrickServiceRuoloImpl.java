c2115888202fc564c36e8c4bf6fdda616434874f
daniela <daniela.punzi@edu-its.it>
2025-11-15 17:39:38 +0100

    implementazione di RuoliREST + modifica del service

--- Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/services/BrickServiceRuoloImpl.java
+++ Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/services/BrickServiceRuoloImpl.java
@@ -1,6 +1,7 @@
 package com.brickbybrick.brickbybrick.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,13 +10,33 @@ import com.brickbybrick.brickbybrick.model.Ruolo;
 import com.brickbybrick.brickbybrick.repos.BrickRepoRuolo;
 
 @Service
-public class BrickServiceRuoloImpl implements BrickServiceRuolo 
-{
+public class BrickServiceRuoloImpl implements BrickServiceRuolo {
+    
     @Autowired
     private BrickRepoRuolo repoRuolo;
 
     @Override
     public List<Ruolo> getRuoli() {
         return repoRuolo.findAll();
+    }
+
+    @Override
+    public Optional<Ruolo> getRuoloById(Integer id) {
+        return repoRuolo.findById(id);
+    }
+
+    @Override
+    public Ruolo saveRuolo(Ruolo ruolo) {
+        return repoRuolo.save(ruolo);
+    }
+
+    @Override
+    public void deleteRuolo(Integer id) {
+        repoRuolo.deleteById(id);
+    }
+
+    @Override
+    public boolean existsById(Integer id) {
+        return repoRuolo.existsById(id);
     }
 }
