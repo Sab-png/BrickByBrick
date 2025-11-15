1c47fd279b6b71c0109966528014192827769e98
daniela <daniela.punzi@edu-its.it>
2025-11-15 17:19:31 +0100

    implementazione di UtenteREST + modifica del service

--- Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/services/BrickServiceUtenteImpl.java
+++ Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/services/BrickServiceUtenteImpl.java
@@ -1,6 +1,7 @@
 package com.brickbybrick.brickbybrick.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,25 @@ public class BrickServiceUtenteImpl implements BrickServiceUtente {
     @Override
     public List<Utente> getUtenti() {
         return repoUtente.findAll();
+    }
+
+    @Override
+    public Optional<Utente> getUtenteById(Integer id) {
+        return repoUtente.findById(id);
+    }
+
+    @Override
+    public Utente saveUtente(Utente utente) {
+        return repoUtente.save(utente);
+    }
+
+    @Override
+    public void deleteUtente(Integer id) {
+        repoUtente.deleteById(id);
+    }
+
+    @Override
+    public boolean existsById(Integer id) {
+        return repoUtente.existsById(id);
     }
 }
