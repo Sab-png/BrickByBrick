677e95f685ca1c628d8477dbd84c581b6f7fbce9
Jacopo Dell'Oste <jacodell25@gmail.com>
2025-11-16 17:14:33 +0100

    feat: immobili page completed

--- Desktop/BrickByBrick/frontend/src/pages/Immobili.jsx
+++ Desktop/BrickByBrick/frontend/src/pages/Immobili.jsx
@@ -1,5 +1,20 @@
+import HeroSection from '../components/HeroSection';
+import Filters from '../components/Filters';
+import ImmobiliResults from '../components/ImmobiliResults';
+import Newsletter from '../components/NewsLetter';
+
+
 export default function Immobili() {
-    return (
-        <h1>Qui ci saranno gli immobili e la ricerca per filtri</h1>
-    )
+
+  return (
+    <>
+      <HeroSection title="Trova la casa dei tuoi sogni!" subtitle="Inizia ora la ricerca" showCTA={false} bgImage="/src/assets/images/hero-immobili.jpg" />
+
+      <Filters />
+
+      <ImmobiliResults />
+
+      <Newsletter />
+    </>
+  );
 }
