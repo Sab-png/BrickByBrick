dc5dbc990fb358be82ce038b516bf1d0266a3666
MarcoSpeda <marco.spedaliere@edu-its.it>
2025-11-17 10:14:46 +0100

    fix branch Marco

--- Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/services/BrickServiceAdminImpl.java
+++ Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/services/BrickServiceAdminImpl.java
@@ -9,15 +9,35 @@ import org.springframework.stereotype.Service;
 import com.brickbybrick.brickbybrick.model.Admin;
 import com.brickbybrick.brickbybrick.repos.BrickRepoAdmin;
 
-@Service
-public class BrickServiceAdminImpl implements BrickServiceAdmin {
+    @Service
+    public class BrickServiceAdminImpl implements BrickServiceAdmin {
 
-    @Autowired
+     @Autowired
     private BrickRepoAdmin repoAdmin;
 
     @Override
     public List<Admin> getAdmins() {
         return repoAdmin.findAll();
+    }
+
+    @Override
+    public Optional<Admin> getAdminById(Integer id) {
+        return repoAdmin.findById(id);
+    }
+
+    @Override
+    public void deleteAdmin(Integer id) {
+        repoAdmin.deleteById(id);
+    }
+
+    @Override
+    public boolean existsById(Integer id) {
+        return repoAdmin.existsById(id);
+    }
+
+    @Override
+    public Admin saveAdmin(Admin admin) {
+        return repoAdmin.save(admin);
     }
     
 }
