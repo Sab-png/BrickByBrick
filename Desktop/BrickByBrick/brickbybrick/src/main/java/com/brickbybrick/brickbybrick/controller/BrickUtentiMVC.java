ebe93fa357fbf871038f5f289f74f79b45b78543
daniela <daniela.punzi@edu-its.it>
2025-11-17 20:42:16 +0100

    aggiunta del CrossOrigin ai rest + fix vari

--- Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/controller/BrickUtentiMVC.java
+++ Desktop/BrickByBrick/brickbybrick/src/main/java/com/brickbybrick/brickbybrick/controller/BrickUtentiMVC.java
@@ -3,7 +3,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.brickbybrick.brickbybrick.model.Agente;
+import com.brickbybrick.brickbybrick.model.Utente;
 import com.brickbybrick.brickbybrick.repos.BrickRepoUtente;
 import com.brickbybrick.brickbybrick.services.BrickServiceUtente;
 
@@ -25,4 +29,16 @@ public class BrickUtentiMVC {
         m.addAttribute("utenti", serviceUtente.getUtenti());
         return "Utenti";
     }
+    
+    // @GetMapping("/utenti/add")
+    // public String showForm(Model model) {
+    //     model.addAttribute("utente", new Utente());
+    //     return "UtentiAdd";
+    // }
+
+    // @PostMapping("/utenti/add")
+    // public String processForm(@ModelAttribute Utente utente) {
+    //     serviceUtente.addUtente(utente);
+    //     return "redirect:/utenti";
+    // }
 }
