956435263578386fd30187563869d34bc17b3b13
Jacopo Dell'Oste <jacodell25@gmail.com>
2025-11-26 13:11:32 +0100

    refactor: added new hero section component to the Immobili page

--- Desktop/BrickByBrick/frontend/src/pages/Immobili.jsx
+++ Desktop/BrickByBrick/frontend/src/pages/Immobili.jsx
@@ -3,12 +3,25 @@ import Filters from '../components/Filters';
 import ImmobiliResults from '../components/ImmobiliResults';
 import Newsletter from '../components/NewsLetter';
 
+import immobiliBgJpg from "../assets/images/jpg/hero-immobili.jpg";
+import immobiliBgWebP from "../assets/images/webp/hero-immobili.webp";
+import immobiliBgAvif from "../assets/images/avif/hero-immobili.avif";
 
 export default function Immobili() {
 
   return (
     <>
-      <HeroSection title="Trova la casa dei tuoi sogni!" subtitle="Inizia ora la ricerca" showCTA={false} bgImage="/src/assets/images/hero-immobili.jpg" />
+      <HeroSection 
+          title={"Trova la casa dei tuoi sogni!"}
+          subtitle="Inizia ora la ricerca"
+          showCTA={false}
+
+          imageDesktop={immobiliBgJpg}       // Fallback JPG
+          imageDesktopWebP={immobiliBgWebP}  // Versione WebP
+          imageDesktopAvif={immobiliBgAvif}  // Versione AVIF
+          imageMobile={immobiliBgWebP}       
+          alt="Panorama di Torino con vista sulla Mole Antonelliana, sfondo per servizio valutazione immobiliare" 
+      />    
 
       <Filters />
 
