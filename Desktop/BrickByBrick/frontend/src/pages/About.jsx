997f56bee7a8863bcf8e9ab8e7c058b8976ed388
Jacopo Dell'Oste <jacodell25@gmail.com>
2025-11-26 13:11:07 +0100

    refactor: added new hero section component to the About page

--- Desktop/BrickByBrick/frontend/src/pages/About.jsx
+++ Desktop/BrickByBrick/frontend/src/pages/About.jsx
@@ -4,11 +4,24 @@ import MissionSection from "../components/MissionSection"
 import StorySection from "../components/StorySection"
 import AgentsSection from "../components/AgentSection"
 
+import aboutBgJpg from "../assets/images/jpg/hero-about.jpg";
+import aboutBgWebP from "../assets/images/webp/hero-about.webp";
+import aboutBgAvif from "../assets/images/avif/hero-about.avif";
+
 export default function About() {
     return (
         <div className="about-page">
+            <HeroSection 
+                title={"Vendi la tua casa in Piemonte con un valore garantito e senza stress."}
+                subtitle="Acquisiamo e vendiamo immobili in esclusiva con metodo digitale e competenza territoriale"
+                showCTA={false}
 
-            <HeroSection title="Esperti locali, risultati misurabili" subtitle="Acquisiamo e vendiamo immobili in esclusiva con metodo digitale e competenza territoriale" showCTA={false} bgImage="/src/assets/images/hero-about.jpg"/> 
+                imageDesktop={aboutBgJpg}       // Fallback JPG
+                imageDesktopWebP={aboutBgWebP}  // Versione WebP
+                imageDesktopAvif={aboutBgAvif}  // Versione AVIF
+                imageMobile={aboutBgWebP}       
+                alt="Panorama di Torino con vista sulla Mole Antonelliana, sfondo per servizio valutazione immobiliare" 
+            />            
 
             <StorySection />
 
