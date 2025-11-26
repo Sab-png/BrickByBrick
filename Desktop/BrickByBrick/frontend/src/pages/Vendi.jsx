10fab83483c752c890e9075f69a6e1c9682a8638
Jacopo Dell'Oste <jacodell25@gmail.com>
2025-11-26 13:11:52 +0100

    refactor: added new hero section component to the Vendi page

--- Desktop/BrickByBrick/frontend/src/pages/Vendi.jsx
+++ Desktop/BrickByBrick/frontend/src/pages/Vendi.jsx
@@ -4,10 +4,23 @@ import Timeline from '../components/Timeline';
 import PrenotaVisita from '../components/PrenotaVisita';
 import Newsletter from '../components/NewsLetter';
 
+import vendiBgJpg from "../assets/images/jpg/hero-vendi.jpg";
+import vendiBgWebP from "../assets/images/webp/hero-vendi.webp";
+import vendiBgAvif from "../assets/images/avif/hero-vendi.avif";
+
 export default function Vendi() {
     return (
             <>
-            <HeroSection title={"Vendi la tua casa in modo chiaro, veloce e sicuro"} subtitle={"Con Immobiliaris hai un team locale, strumenti digitali avanzati e una valutazione reale in 72 ore."} bgImage={"/src/assets/images/hero-vendi.avif"}/>
+            <HeroSection 
+                title={"Vendi la tua casa in modo chiaro, veloce e sicuro"}
+                subtitle="Con Immobiliaris hai un team locale, strumenti digitali avanzati e una valutazione reale in 72 ore."
+                // Passiamo i file importati ai rispettivi props
+                imageDesktop={vendiBgJpg}       // Fallback JPG
+                imageDesktopWebP={vendiBgWebP}  // Versione WebP
+                imageDesktopAvif={vendiBgAvif}  // Versione AVIF
+                imageMobile={vendiBgWebP}       
+                alt="Panorama di Torino con vista sulla Mole Antonelliana, sfondo per servizio valutazione immobiliare" 
+            />
 
             <VendiTech />
 
