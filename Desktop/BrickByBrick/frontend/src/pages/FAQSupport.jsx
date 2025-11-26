365b629ebdde1d16256d2e25937dab763f213b29
Jacopo Dell'Oste <jacodell25@gmail.com>
2025-11-26 13:11:23 +0100

    refactor: added new hero section component to the Support page

--- Desktop/BrickByBrick/frontend/src/pages/FAQSupport.jsx
+++ Desktop/BrickByBrick/frontend/src/pages/FAQSupport.jsx
@@ -3,10 +3,24 @@ import FAQSection from "../components/FAQSection"
 import Questions from "../components/Questions"
 import Newsletter from "../components/NewsLetter"
 
+import supportoBgJpg from "../assets/images/jpg/hero-supporto.jpg";
+import supportoBgWebP from "../assets/images/webp/hero-supporto.webp";
+import supportoBgAvif from "../assets/images/avif/hero-supporto.avif";
+
 export default function FAQSupport() {
     return (
         <>
-        <HeroSection title="Supporto Immobiliaris: siamo qui per aiutarti a casa." subtitle="Esplora le nostre FAQ o contattaci direttamente" showCTA={false} bgImage="/src/assets/images/hero-supporto.jpg"/>
+        <HeroSection 
+            title={"Supporto Immobiliaris: siamo qui per aiutarti a casa."}
+            subtitle="Esplora le nostre FAQ o contattaci direttamente"
+            showCTA={false}
+
+            imageDesktop={supportoBgJpg}       // Fallback JPG
+            imageDesktopWebP={supportoBgWebP}  // Versione WebP
+            imageDesktopAvif={supportoBgAvif}  // Versione AVIF
+            imageMobile={supportoBgWebP}       
+            alt="Panorama di Torino con vista sulla Mole Antonelliana, sfondo per servizio valutazione immobiliare" 
+        />    
         
         <FAQSection />
 
