517fae63069a67219fc9a11daf774c7256e16b89
Jacopo Dell'Oste <jacodell25@gmail.com>
2025-12-02 17:53:22 +0100

    feat: added correct controls

--- Desktop/BrickByBrick/frontend/src/components/ProtectedRoute.jsx
+++ Desktop/BrickByBrick/frontend/src/components/ProtectedRoute.jsx
@@ -13,9 +13,11 @@ const ProtectedRoute = ({ Component, requiredRole = null }) => {
   }
 
   if (requiredRole && user?.role !== requiredRole) {
+    // se l'utente è autenticato ma non ha il ruolo richiesto -> home
     return <Navigate to="/" replace />;
   }
 
+  // Component può essere un layout o una pagina; restituiscilo così com'è
   return <Component />;
 };
 
